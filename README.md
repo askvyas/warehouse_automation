b5408940e568d93b0562c2f980bc594d784a782d
askvyas <vyasakondi009@gmail.com>
2024-01-23 15:09:39 -0500

    add turtlebot3 and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-#ROS2 Warehouse Automation
+# Warehouse Automation with TurtleBot3 in ROS2
+
+## Overview
+This project integrates TurtleBot3 with the AWS RoboMaker Small Warehouse World in a ROS2 and Gazebo simulation environment.
+
+Prerequisites
+ROS2 (Humble)
+Gazebo
+TurtleBot3 Gazebo package
+
+## Setup
+1. Clone AWS RoboMaker Warehouse Repository
+'''
+git clone https://github.com/aws-robotics/aws-robomaker-small-warehouse-world
+'''
+2. Source ROS2 and Gazebo
+'''
+source /opt/ros/humble/setup.bash
+source /usr/share/gazebo/setup.sh
+'''
+3. Install TurtleBot3
+'''
+sudo apt-get install ros-humble-turtlebot3-gazebo
+'''
+4. Set TurtleBot3 Model
+'''
+export TURTLEBOT3_MODEL=waffle
+'''
+5. Create ROS2 Package
+'''
+ros2 pkg create warehouse_automation --build-type ament_cmake --dependencies rclcpp rclpy
+'''
+
+## Launch the Simulation
+
+'''
+ros2 launch warehouse_automation no_roof_small_warehouse.launch.py
+
+'''
+
+
+## Common Issues 
+Error: Gzclient process crash with exit code -6.
+
+Solution: Source the Gazebo setup file.
+
+'''
+source /usr/share/gazebo/setup.sh
+
+'''
+
+reference : https://answers.ros.org/question/358847/cannot-launch-gzclient-on-a-launch-file-results-in-shared_ptr-assertion-error/
--- launch/no_roof_small_warehouse.launch.py
+++ launch/no_roof_small_warehouse.launch.py
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Launch file from AWS warehouse gazebo environment 
+
+# Updated to launch turtle bot3
+
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -22,15 +27,26 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
+    aws_robomaker_small_warehouse_world_dir = get_package_share_directory('aws_robomaker_small_warehouse_world')
+
+    turtlebot3_gazebo_dir = get_package_share_directory('turtlebot3_gazebo')
+    turtlebot3_launch_file_path='/opt/ros/humble/share/turtlebot3_gazebo/launch/spawn_turtlebot3.launch.py'
+    warehouse_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([aws_robomaker_small_warehouse_world_dir, '/launch/small_warehouse.launch.py']),
+        launch_arguments={
+            'world': os.path.join(aws_robomaker_small_warehouse_world_dir, 'worlds', 'no_roof_small_warehouse', 'no_roof_small_warehouse.world')
+        }.items()
+    )
+
+    turtlebot_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(turtlebot3_launch_file_path)
+    )
+
+
+
     ld = launch.LaunchDescription([
-        launch.actions.IncludeLaunchDescription(
-            launch.launch_description_sources.PythonLaunchDescriptionSource(
-                [get_package_share_directory(
-                    'aws_robomaker_small_warehouse_world'), '/launch/small_warehouse.launch.py']
-            ),
-            launch_arguments={
-                'world': os.path.join(get_package_share_directory('aws_robomaker_small_warehouse_world'), 'worlds', 'no_roof_small_warehouse', 'no_roof_small_warehouse.world')
-            }.items()
-        )
+        warehouse_launch,
+        turtlebot_launch
     ])
+
     return ld
